f4d6d4cf631701d950ec6d22da60031906416f99
TexFR <Texsword76@gmail.com>
2022-10-12 22:33:13 +0200

    k
    k

--- src/index.php
+++ src/index.php
@@ -1,20 +1,22 @@
 <?php
+ini_set('display_errors', 1);
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "amahzone";
+$database = "amahzone";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+$connection = new mysqli($servername, $username, $password, $database);
+
+if ($connection->connect_error) {
+  echo "nope:";
 }
 
-$sql = "SELECT * FROM `article` WHERE 1";
-$result = $conn->query($sql);
+$sql = "SELECT * FROM article";
+$result = $connection->query($sql);
 
-$conn->close();
+if (!$result){
+  echo "invalid q";
+}
 ?>
 
 <!DOCTYPE html>
@@ -27,29 +29,101 @@ $conn->close();
     <title>Amahzone</title>
 </head>
 <body>
-    <h1 class="bg-gre text-center"><?php 
-    if ($result->num_rows > 0) {
-        // output data of each row
-        while($row = $result->fetch_assoc()) {
-          echo "id: " . $row["id"]. " - Name: " . $row["libele"]. " " . $row["description"]. " " .  $row["prixttc"]. " " . $row["repimage"]. "<br>";
-        }
-      } else {
-        echo "0 results";
-      }
-    
-    ?>
-</h1>
+
+
+<nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+  <div class="container flex flex-wrap justify-between items-center mx-auto">
+  <a href="https://flowbite.com/" class="flex items-center">
+      <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo">
+      <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+  </a>
+  <div class="flex md:order-2">
+      <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
+      <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
+        <span class="sr-only">Open main menu</span>
+        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+    </button>
+  </div>
+  <div class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="navbar-cta">
+    <ul class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+      <li>
+        <a href="#" class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
+      </li>
+      <li>
+        <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+      </li>
+      <li>
+        <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+      </li>
+      <li>
+        <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+      </li>
+    </ul>
+  </div>
+  </div>
+</nav>
+
+
+<div>
+<div class="ml-auto mr-auto">
+  
+
+<h2 class="text-4xl font-bold dark:text-white">Nos Smartphones.</h2>
+<div class="grid py-12 grid-cols-4 gap-10 max-w-[1200px]">
+
 <?php
 
-    foreach ($result as $key=>$result) {
-        if($key % 2 == 0){
-            echo '<div class="grid grid-rows-4 grid-flow-col gap-4">';
-            echo '<div class="elementcontent">'. $result . '</div>';
-        } else {
-            echo '<div class="elementcontent">'. $result . '</div>';
-            echo '</div>';
-        }
-    }
+while($row = $result->fetch_assoc()){
+ echo "<div class='w-60 h-80 rounded-lg bg-gray-50 drop-shadow-lg flex flex-col justify-center'>
+  <div class='mr-auto ml-auto mt-2'>
+    <img class='aspect-auto w-48' src=" . $row["repimage"] . " alt=''>
+  </div>
+  <dir class='flex flex-col pl-5 pr-5'>
+    <div>
+      <h3>" . $row["libele"] . "</h3>
+    </div>
+    <div>
+      <h3 class='text-2xl font-semibold'>" . $row["prixu"] . " €" ."</h3>
+    </div>
+  </dir> 
+</div>";
+
+}
+
 ?>
+
+
+</div>
+</div>
+</div>
+
+
+
+<footer class="p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-900">
+    <div class="sm:flex sm:items-center sm:justify-between">
+        <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0">
+            <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-8" alt="Flowbite Logo">
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+        </a>
+        <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
+            <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+            </li>
+            <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+            </li>
+            <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6 ">Licensing</a>
+            </li>
+            <li>
+                <a href="#" class="hover:underline">Contact</a>
+            </li>
+        </ul>
+    </div>
+    <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8">
+</footer>
+
+
 </body>
+
 </html>
