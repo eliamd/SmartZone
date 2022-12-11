f541f53ae531c99b56cf8e9f4a58e70c7a28e539
TexFR <Texsword76@gmail.com>
2022-12-11 13:30:45 +0100

    vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'eliam/amahzonee',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '144d9ff65976fac95f92710170b527a6093e8e0f',
+        'type' => 'projet',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'eliam/amahzonee' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '144d9ff65976fac95f92710170b527a6093e8e0f',
+            'type' => 'projet',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v10.0.0',
+            'version' => '10.0.0.0',
+            'reference' => 'a895466e5208cc7974bc3bed80abe825418ce0b0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
